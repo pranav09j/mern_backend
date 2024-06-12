06c5f5a2401d88f1cb24faadd91319a0cd71a4b4
pranav <pranav09j@gmail.com>
2024-06-12 12:29:50 +0530

    user controller and routing and multer

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-    app.on((error)=>{
-        console.log("Error :",error);
-        throw error;
-    })
+   
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
